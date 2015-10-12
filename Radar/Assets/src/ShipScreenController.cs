fdacdae9edeb5649b71d1ebe66617b4665e4a8f3
Stas Shostak <messiastas@gmail.com>
2015-10-12 12:05:08 +0300

    11.10 changes

---
+++ Radar/Assets/src/ShipScreenController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipScreenController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        onStart();
+    }
+
+    protected virtual void onStart()
+    {
+
+    }
+
+        // Update is called once per frame
+        void Update () {
+	
+	}
+}
